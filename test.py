43b2c1fd4e10100a9692d37c9685bff1c8aad3e5
Jiale <jiale.tan@gllue.com>
2020-09-11 14:33:58 +0800

    修改文件名

--- test.py
+++
@@ -1,8 +0,0 @@
-import datetime
-import time
-
-
-start = datetime.datetime.now() - datetime.timedelta(days=30)
-start = int(time.mktime(start.timetuple()))
-end = int(time.time())
-print(start, end)
