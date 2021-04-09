a3ec40c9ce4f370726738b9f8f4ceb37f55a6be4
Jiale <1531391246@qq.com>
2021-04-09 08:35:26 +0800

    leetcode-algorithm-154-寻找旋转排序数组中的最小值II

--- leetcode/algorithm/154：寻找旋转排序数组中的最小值II.py
+++ leetcode/algorithm/154：寻找旋转排序数组中的最小值II.py
@@ -1,4 +1,6 @@
 from typing import List
+
+
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         n = len(nums)
@@ -10,7 +12,14 @@ class Solution:
             mid = (l + r) // 2
             if nums[mid] < nums[r]:
                 r = mid
-            else:
+            elif nums[mid] > nums[r]:
                 l = mid + 1
+            else:
+                r -= 1
 
         return nums[l]
+
+
+if __name__ == '__main__':
+
+    print(Solution().findMin([3,3,1,3]))
