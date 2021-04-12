5d6ebd1f007b2bd6c654d130e3441aff0456f3b2
Jiale <1531391246@qq.com>
2021-04-12 22:58:00 +0800

    leetcode-algorithm-179-最大数

--- leetcode/algorithm/179：最大数.py
+++ leetcode/algorithm/179：最大数.py
@@ -3,24 +3,25 @@ from typing import List
 
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        self.quick_sort(nums=nums, l=0, r=len(nums) - 1)
         nums = [str(num) for num in nums]
+        self.quick_sort(nums=nums, l=0, r=len(nums) - 1)
+        if nums[0] == '0':
+            return '0'
         return ''.join(nums)
 
-    def quick_sort(self, nums: List[int], l, r):
-        num = nums[l]
+    def quick_sort(self, nums: List[str], l, r):
         i, j = l, r
         if i>=j:
             return
+        num = nums[l]
         while i < j:
-            while i < j and int(str(nums[j]) + str(num)) <= int(str(num) + str(nums[j])):
+            while i < j and int(nums[j] + num) <= int(num + nums[j]):
                 j -= 1
-            while i < j and int(str(nums[i]) + str(num)) >= int(str(num) + str(nums[i])):
+            while i < j and int(nums[i] + num) >= int(num + nums[i]):
                 i += 1
             if i < j:
                 nums[i], nums[j] = nums[j], nums[i]
-        nums[l] = nums[i]
-        nums[i] = num
+        nums[l], nums[i] = nums[i], num
         self.quick_sort(nums, l, i-1)
         self.quick_sort(nums, i+1, r)
 
