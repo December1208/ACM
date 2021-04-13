831d5bc84ff326c3116ea882437e669e0f08f104
Jiale <1531391246@qq.com>
2021-04-13 08:43:46 +0800

    leetcode-algorithm-783-二叉搜索树节点最小距离 error

---
+++ leetcode/algorithm/783：二叉搜索树节点最小距离.py
@@ -0,0 +1,26 @@
+from math import inf
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def minDiffInBST(self, root: TreeNode) -> int:
+        self.result, self.value = inf, -1
+        self.dfs(root)
+        return self.result
+
+    def dfs(self, root: TreeNode):
+        if root is None:
+            return
+        self.dfs(root.left)
+        if self.value == -1:
+            self.value = root.val
+        else:
+            self.result = min(self.result, root.val - self.value)
+            self.value = root.val
+        self.dfs(root.left)
