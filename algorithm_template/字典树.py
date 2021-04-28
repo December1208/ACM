d4851594247b9a490de48b474d4b44826aca83c7
December <1531391246@qq.com>
2021-04-28 18:13:10 +0800

    algorithm_template-字典树

--- algorithm_template/字典树.py
+++ algorithm_template/字典树.py
@@ -13,6 +13,9 @@ class Solution:
 
     def __init__(self):
         self.root = Node()
+        self.chara = [[0 for i in range(26)] for j in range(200001)]
+        self.vis = [0 for i in range(200001)]
+        self.e = 1
 
     def insert(self, word):
         root = self.root
@@ -38,6 +41,27 @@ class Solution:
         if root.is_end:
             return True
         return False
+
+    def insert_list(self, word):
+        p = 1
+        for char in word:
+            index = ord(char) - ord('a')
+            if self.chara[p][index] == 0:
+                self.e += 1
+                self.chara[p][index] = self.e
+                self.vis[self.e] = 0
+            p = self.chara[p][index]
+            self.vis[p] += 1
+
+    def search_list(self, word):
+        p = 1
+        for char in word:
+            index = ord(char) - ord('a')
+            if self.chara[p][index] == 0:
+                return False
+            p = self.chara[p][index]
+
+        return self.vis[p]
 
 
 if __name__ == '__main__':
